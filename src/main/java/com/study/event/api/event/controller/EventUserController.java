f898a9e951f0f686a6c1de6f6b578c38c78f302d
soongu <naong526@naver.com>
2024-07-15 09:57:45 +0900

    # 9. 간단한 로그인 검증

--- src/main/java/com/study/event/api/event/controller/EventUserController.java
+++ src/main/java/com/study/event/api/event/controller/EventUserController.java
@@ -1,11 +1,16 @@
 package com.study.event.api.event.controller;
 
 import com.study.event.api.event.dto.request.EventUserSaveDto;
+import com.study.event.api.event.dto.request.LoginRequestDto;
 import com.study.event.api.event.service.EventUserService;
+import com.study.event.api.exception.LoginFailException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/auth")
@@ -48,6 +53,20 @@ public class EventUserController {
         return ResponseEntity.ok().body("saved success");
     }
 
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> signIn(@RequestBody LoginRequestDto dto) {
+
+        try {
+            eventUserService.authenticate(dto);
+            return ResponseEntity.ok().body("login success");
+        } catch (LoginFailException e) {
+            // 서비스에서 예외발생 (로그인 실패)
+            String errorMessage = e.getMessage();
+            return ResponseEntity.status(422).body(errorMessage);
+        }
+
+    }
 
 
 }
--- src/main/java/com/study/event/api/event/service/EventUserService.java
+++ src/main/java/com/study/event/api/event/service/EventUserService.java
@@ -1,10 +1,12 @@
 package com.study.event.api.event.service;
 
 import com.study.event.api.event.dto.request.EventUserSaveDto;
+import com.study.event.api.event.dto.request.LoginRequestDto;
 import com.study.event.api.event.entity.EmailVerification;
 import com.study.event.api.event.entity.EventUser;
 import com.study.event.api.event.repository.EmailVerificationRepository;
 import com.study.event.api.event.repository.EventUserRepository;
+import com.study.event.api.exception.LoginFailException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,10 +44,34 @@ public class EventUserService {
         boolean exists = eventUserRepository.existsByEmail(email);
         log.info("Checking email {} is duplicate : {}", email, exists);
 
+        // 중복인데 회원가입이 마무리되지 않은 회원은 중복이 아니라고 판단
+        if (exists && notFinish(email)) {
+            return false;
+        }
+
         // 일련의 후속 처리 (데이터베이스 처리, 이메일 보내는 것...)
         if (!exists) processSignUp(email);
 
         return exists;
+    }
+
+
+    private boolean notFinish(String email) {
+        EventUser eventUser = eventUserRepository.findByEmail(email).orElseThrow();
+
+        if (!eventUser.isEmailVerified() || eventUser.getPassword() == null) {
+            // 기존 인증코드가 있는 경우 삭제
+            EmailVerification ev = emailVerificationRepository
+                                    .findByEventUser(eventUser)
+                                    .orElse(null);
+
+            if (ev != null) emailVerificationRepository.delete(ev);
+
+            // 인증코드 재발송
+            generateAndSendCode(email, eventUser);
+            return true;
+        }
+        return false;
     }
 
     public void processSignUp(String email) {
@@ -174,4 +200,34 @@ public class EventUserService {
         foundUser.confirm(encodedPassword);
         eventUserRepository.save(foundUser);
     }
+
+
+
+    // 회원 인증 처리 (login)
+    public void authenticate(final LoginRequestDto dto) {
+
+        // 이메일을 통해 회원정보 조회
+        EventUser eventUser = eventUserRepository.findByEmail(dto.getEmail())
+                .orElseThrow(
+                        () -> new LoginFailException("가입된 회원이 아닙니다.")
+                );
+
+        // 이메일 인증을 안했거나 패스워드를 설정하지 않은 회원
+        if (!eventUser.isEmailVerified() || eventUser.getPassword() == null) {
+            throw new LoginFailException("회원가입이 중단된 회원입니다. 다시 가입해주세요.");
+        }
+
+        // 패스워드 검증
+        String inputPassword = dto.getPassword();
+        String encodedPassword = eventUser.getPassword();
+
+        if (!encoder.matches(inputPassword, encodedPassword)) {
+            throw new LoginFailException("비밀번호가 틀렸습니다.");
+        }
+
+        // 로그인 성공
+        // 인증정보를 어떻게 관리할 것인가?
+
+    }
+
 }
