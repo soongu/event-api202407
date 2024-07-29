9f9295600f180540074a7eac0681f98c979b16c3
soongu <naong526@naver.com>
2024-07-29 16:19:02 +0900

    # ci/cd 1

---
+++ src/main/java/com/study/event/api/HomeController.java
@@ -0,0 +1,13 @@
+package com.study.event.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "server is running...";
+    }
+}
