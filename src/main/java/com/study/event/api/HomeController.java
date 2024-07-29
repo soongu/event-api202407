c8c367dcf681ad525100fe10985dad0a8f684ae9
soongu <naong526@naver.com>
2024-07-29 17:56:49 +0900

    # ci/cd 15

--- src/main/java/com/study/event/api/HomeController.java
+++ src/main/java/com/study/event/api/HomeController.java
@@ -2,8 +2,9 @@ package com.study.event.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
